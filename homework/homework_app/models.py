18a205eaa5e03fd1dcb1b0b8ba9b80369d1fd2d2
omele4ka <omele4ka@inbox.ru>
2024-01-27 15:23:24 -0500

    model Item is changed. Added field to save product's pictures

--- homework/homework_app/models.py
+++ homework/homework_app/models.py
@@ -18,6 +18,7 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     add_date = models.DateField(auto_now_add=True)
+    picture = models.ImageField(upload_to='item_pic/', null=True, blank=True)
 
     def __str__(self):
         return f'Item {self.title} {self.description} {self.price}'
