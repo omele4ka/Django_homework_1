93b4844ace32672364d7d31c74ba7b7884e7b655
omele4ka <omele4ka@inbox.ru>
2024-01-31 19:13:29 -0500

    administrative panel is created

--- homework/homework_app/admin.py
+++ homework/homework_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Item, Order
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'quantity']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'total_price']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Order, OrderAdmin)
